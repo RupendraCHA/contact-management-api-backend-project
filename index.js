9e82b984d998c8dc651dfd5d5d5e2f005cd295f8
RupendraCHA <chandaluri210@gmail.com>
2025-02-11 11:08:15 +0530

    Adding web icon and server starting text with HTML Elements as a response

--- index.js
+++ index.js
@@ -13,11 +13,41 @@ app.use(express.json())
 
 app.use(cors())
 
-app.get("/", (req, res) => {
-    res.send("Api is working")
-})
 
 app.use("/api/v1", contactRouter)
+
+app.get("/start", (req, res) => {
+    res.send(`
+          <!DOCTYPE html>
+          <html>
+          <head>
+          <link
+        rel="icon"
+        type="image/svg+xml"
+        href="https://res.cloudinary.com/dvxkeeeqs/image/upload/v1739252237/AAPI_y3xqk8.webp"
+      />
+            <title>Contact Management API server</title>
+            <style>
+              div{
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                  height: 100vh;
+                  background-color: black;
+              }
+              h1{
+                  color: white;
+              }
+            </style>
+          </head>
+          <body>
+              <div>
+                  <h1>Contact Management API Server running Successfully</h1>
+              </div>
+          </body>
+          </html>
+        `);
+  });
 
 // defining and assigning PORT
 const port = process.env.PORT
